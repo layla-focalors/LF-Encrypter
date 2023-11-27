dc8168032fd59dba938b3707f465978847c201c9
arisia <starlights2283@gmail.com>
2023-11-27 14:13:41 +0900

    add files

--- lfincrypter.py
+++ lfincrypter.py
@@ -20,7 +20,32 @@ def GetENV():
     import os
     return os.environ['LF-ENC']
 
-def GetEncrypt(text):
-    
-    return 9
+def CustomGetENV(cust:str):
+    import os
+    return os.environ[f'{cust}']
 
+def CustomEncrypt(hash_text : str, env_name):
+    import hashlib, os
+    hash_text += os.environ[f'{env_name}']
+    print(hash_text)
+    byte_data = hash_text.encode("UTF-8")
+    hash_object = hashlib.sha256(byte_data)
+    return hash_object.hexdigest()
+
+def Encrypt(hash_text : str):
+    import hashlib, os
+    hash_text += os.environ['LF-ENC']
+    byte_data = hash_text.encode("UTF-8")
+    hash_object = hashlib.sha256(byte_data)
+    return hash_object.hexdigest()
+
+def EncyptChecker(hash : str, userText : str):
+    import hashlib, os
+    userText += os.environ['LF-ENC']
+    byte_data = userText.encode("UTF-8")
+    hash_object = hashlib.sha256(byte_data)
+    hash2 = hash_object.hexdigest()
+    if hash == hash2:
+        return True
+    else:
+        return False
